843878d4fbfcec0b47df0272cb6bb2fecb8cd54c
AEpsiuq <aliss.q1i5@gmail.com>
2026-03-27 16:10:17 -0300

    Add files via upload

---
+++ Usuario y cine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usuario_y_cine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaracion de variablee
+
+            string nombre;
+            string pelicula;
+
+
+            Console.WriteLine("Hola");
+            Console.Write("Como te llamas?: ");
+
+            nombre = Console.ReadLine();
+
+            Console.Write("Cual es tu peli favorita?: ");
+
+            pelicula = Console.ReadLine();
+
+            Console.WriteLine("Hola " + nombre + ". Tu pelicula preferida " + pelicula + " es una porqueria ");
+
+        }
+    }
+}
