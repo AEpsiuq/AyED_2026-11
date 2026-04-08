07a01ecdd9010c2f24c36b85ebb08d45a7e16182
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-08 14:33:53 -0300

    Add files via upload

---
+++ Calculo edad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operacionaritmetica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¿Que año naciste?: ");
+            int año = Convert.ToInt32(Console.ReadLine());
+            int edad = 2026 - año;
+
+            Console.WriteLine("Si naciste en el año " + año + ", tu edad al iniciar el año es de " + edad + " años");
+
+
+
+        }
+    }
+}
