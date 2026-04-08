318d4bb793b4e7b2e74903a87caa11654444c6c0
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-08 14:27:22 -0300

    Add files via upload

---
+++ OPERADORES 1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operacionaritmetica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar primer valor: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingresar segundo valor: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int suma = a + b;
+            int resta = a - b;
+            int multiplicacion = a * b;
+            int division = a / b;
+
+            Console.WriteLine("Suma: " + suma);
+
+            Console.WriteLine("Resta: " + resta);
+
+            Console.WriteLine("Multiplicacion: " + multiplicacion);
+
+            Console.WriteLine("Division: " + division);
+
+        }
+    }
+}
