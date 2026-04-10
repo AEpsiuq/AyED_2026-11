f8c24641ba6774a76b898deb61306336e21e60ad
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-10 14:27:02 -0300

    Add files via upload

---
+++ ifandelse/ifandelse3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_and_elsse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese distancia (años luz): ");
+            int distancia = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese combustible (litros): ");
+            int combustible = Convert.ToInt32(Console.ReadLine());
+
+            int necesario = distancia * 12;
+
+            if (combustible >= necesario)
+            {
+                Console.WriteLine("Cálculos precisos. Saltando al hiperespacio");
+            }
+            else
+            {
+                int falta = necesario - combustible;
+                Console.WriteLine("Peligro: Combustible insuficiente. Faltan " + falta + " litros");
+            }
+        }
+    
+    }
+}
