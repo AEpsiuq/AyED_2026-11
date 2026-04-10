c8b6eb53c417314bff8944a40f46eb77fb145e67
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-10 14:43:16 -0300

    Add files via upload

---
+++ ifandelse/ifandelse4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_and_elsse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar cantidad de droides: ");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+
+            int precio = 2000;
+            int total = cantidad * precio;
+
+            if (cantidad >= 3)
+            {
+                int descuento = total * 20 / 100;
+                int precio_final = total - descuento;
+
+                Console.WriteLine("Total original: " + total);
+                Console.WriteLine("Descuento: " + descuento);
+                Console.WriteLine("Precio final: " + precio_final);
+            }
+            else
+            {
+                Console.WriteLine("Total: " + total);
+            }
+        }
+    
+    }
+}
