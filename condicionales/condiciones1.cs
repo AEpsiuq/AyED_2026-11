21d3da89d84769bc189f0571f6787f3b28a9b120
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-10 15:28:20 -0300

    Add files via upload

---
+++ condicionales/condiciones1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_and_elsse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar velocidad del vehiculo: ");
+            int velocidad = Convert.ToInt32(Console.ReadLine());
+
+            if (velocidad > 120)
+            {
+                Console.WriteLine("Multa en camino: Exceso de velocidad");
+            }
+            else
+            {
+                Console.WriteLine("Conducción segura");
+            }
+        }
+    }
+}
