52143eafbc2037019ae23b189ec95d6acb64ead1
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-10 16:57:44 -0300

    Add files via upload

---
+++ condicionales/condicionales7.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_and_elsse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar temperatura actual: ");
+
+            int temperatura = Convert.ToInt32(Console.ReadLine());
+
+
+            if ( temperatura < 0)
+            {
+                Console.WriteLine("Peligro de congelmiento");
+            }
+            else
+            {
+                if (temperatura <= 15)
+                {
+                    Console.WriteLine("Mucho frio");
+                }
+                else
+                {
+                    if (temperatura > 15)
+                    {
+                        Console.WriteLine("Clima agradable");
+                    }
+                }
+                  
+            }
+        }
+    }
+
+            
+}
