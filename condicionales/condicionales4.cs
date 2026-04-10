99b216243b06abcf92011d4ceba5944c992660e6
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-10 16:22:35 -0300

    Add files via upload

---
+++ condicionales/condicionales4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_and_elsse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingresar nota: ");
+            int nota = Convert.ToInt32(Console.ReadLine());
+          
+            if (nota >= 7 ) 
+            {
+                Console.WriteLine("Promocionando");
+            }
+            else
+            {
+                if (nota >= 4)
+                {
+                    Console.WriteLine("A finales");
+                }
+                else
+                {
+                    if(nota < 4)
+                    {
+                        Console.WriteLine("Recuperatorio");
+                    }
+                }
+            }
+        }
+    }
+}
