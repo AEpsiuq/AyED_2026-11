3fbb2058f38b5ac6870c1011c60c3abbf3737616
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-10 15:46:52 -0300

    Add files via upload

---
+++ condicionales/condiciones2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_and_elsse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.WriteLine("El club de la Pelea ");
+
+            Console.Write("Ingresar nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingresar edad: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad > 18)
+            {
+                Console.WriteLine("Bienvenido al club, " + nombre);
+            }
+            else
+            {
+                Console.WriteLine("Lo siento, eres muy polluelo para esto");
+            }
+        }
+    }
+}
