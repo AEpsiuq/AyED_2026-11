72598cdad578ad4c0d8b6146a529cc8afd286e01
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-10 14:10:02 -0300

    Add files via upload

---
+++ ifandelse/ifelse1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_and_elsse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar peso en kg: ");
+            int peso = Convert.ToInt32(Console.ReadLine());
+
+            if (peso <= 5000)
+            {
+                Console.WriteLine("Despegue autorizado. Buen viaje, Mando.");
+            }
+            else
+            {
+                int exceso = peso - 5000;
+                Console.WriteLine("Alerta: Exceso de peso. Debes descargar " + exceso + " kg para despegar");
+            }
+        }
+    }
+}
