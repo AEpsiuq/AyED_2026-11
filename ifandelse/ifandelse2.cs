ea3524a08515bd02a085dfa9a0546f87a1898368
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-10 14:19:26 -0300

    Add files via upload

---
+++ ifandelse/ifandelse2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_and_elsse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar cantidad de midiclorianos: ");
+            int midiclorianos = Convert.ToInt32(Console.ReadLine());
+
+            if (midiclorianos > 15000)
+            {
+                Console.WriteLine("Potencial Maestro Jedi detectado. Avisar al Consejo");
+            }
+            else
+            {
+                if (midiclorianos >= 5000)
+                {
+                    Console.WriteLine("Aceptable para entrenamiento Padawan");
+                }
+                else
+                {
+                    Console.WriteLine("No posee sensibilidad a la Fuerza");
+                }
+            }
+        }
+    
+    }
+}
