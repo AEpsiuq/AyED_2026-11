141bb05fa672c58c95c52c891a64b6f79e93eac9
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-15 16:06:28 -0300

    Add files via upload

---
+++ condicionales2/cond13.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string genero;
+
+            Console.Write("Inmgresar genero: ");
+            genero = Console.ReadLine();
+
+            Console.Write("Inmgresar edad: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            if (genero == "F")
+            {
+                if (edad >= 60)
+                {
+                    Console.WriteLine("Puede iniciar el tramite jubilatorio");
+                }
+            }
+            else
+            {
+                if (genero == "M")
+                {
+                    if (edad >= 65)
+                    {
+                        Console.WriteLine("Puede iniciar el tramite jubilatorio");
+                    }
+                }
+            }
+        }                    
+    }
+}
