99cbc0e7a85e0bff5849cf36f0384e6b7a0bc6e9
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-15 16:08:07 -0300

    Add files via upload

---
+++ condicionales/Backup Files/condicionales10.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar peso: ");
+            int peso = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingresar altura: ");
+            double altura = Convert.ToInt32(Console.ReadLine());
+
+            double IMC = peso / (altura * altura);
+
+
+            if (IMC > 25)
+            {
+                Console.WriteLine("Sobrepeso");
+            }
+            else
+            {
+                Console.WriteLine("Rango normal");
+            }
+
+        }
+    }
+}
