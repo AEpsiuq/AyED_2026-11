56c38d6fde6f273e567233930b0393ed88192d32
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-15 14:00:41 -0300

    Add files via upload

---
+++ condicionales/condicionales6.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese usuario: ");
+            string usuario = Console.ReadLine();
+
+            Console.Write("Ingresar contraseña: ");
+            string clave = Console.ReadLine();
+
+            if (usuario == "admin")
+            {
+                if (clave == "1234")
+                {
+                    Console.WriteLine("Acceso concedido");
+                }
+                else
+                {
+                    Console.WriteLine("Error de credenciales");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error de credenciales");
+            }
+        }
+    }
+
+}
+    
+
