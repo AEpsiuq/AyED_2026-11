18e4c7066d0922cc52f839fd3697a4bf46333140
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-24 14:34:18 -0300

    Add files via upload

---
+++ condicionales2/cond17.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar saldo: ");
+            int saldo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingresar monto a retirar: ");
+            int retiro = Convert.ToInt32(Console.ReadLine());
+
+            if (retiro > saldo)
+            {
+                Console.WriteLine("Fondos insuficientes");
+            }
+            else
+            {
+                if (retiro % 1000 == 0)
+                {
+                    Console.WriteLine("Retiro realizado");
+                }
+                else
+                {
+                    Console.WriteLine("Solo billetes de 1000");
+                }
+            }
+        }
+    }
+}
---
+++ condicionales2/cond18.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar horas estacionado: ");
+            int horas = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingresar dinero disponible: ");
+            int dinero = Convert.ToInt32(Console.ReadLine());
+
+            int total;
+
+            if (horas == 1)
+            {
+                total = 4000;
+            }
+            else
+            {
+                total = 4000 + (horas - 1) * 1600;
+            }
+
+            if (dinero >= total)
+            {
+                int cambiuo = dinero - total;
+                Console.WriteLine("Pago realizado. Vuelto: " + cambiuo);
+            }
+            else
+            {
+                int falta = total - dinero;
+                Console.WriteLine("Falta dinero: " + falta);
+            }
+        }
+    }
+}
