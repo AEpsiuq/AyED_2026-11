f3f2637fa1212f0f8443920199fc7d9959e76ae4
AEpsiuq <aliss.q1i5@gmail.com>
2026-04-24 14:34:56 -0300

    Add files via upload

---
+++ condicionales2/cond15.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingresar promedio: ");
+            int promedio = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingresar distancia (km): ");
+            int distancia = Convert.ToInt32(Console.ReadLine());
+
+            if (promedio > 8)
+            {
+                Console.WriteLine("Beca concedida");
+            }
+            else
+            {
+                if (distancia > 50)
+                {
+                    Console.WriteLine("Beca concedida");
+                }
+                else
+                {
+                    Console.WriteLine("No obtiene la beca");
+                }
+            }
+        
+        }
+    }
+}
