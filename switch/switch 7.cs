a298ef9a7298d55c676c3ff09084a3a5a69c57ee
AEpsiuq <aliss.q1i5@gmail.com>
2026-05-08 14:48:26 -0300

    Add files via upload

---
+++ switch/switch 7.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elegí un idioma: ");
+            Console.WriteLine("1. Inglés");
+            Console.WriteLine("2. Francés");
+            Console.WriteLine("3. Alemán");
+
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1 :
+                    Console.WriteLine("Hellooo");
+                    break;
+                case 2 :
+                    Console.WriteLine("Salut");
+                    break;
+                case 3:
+                    Console.WriteLine("Halloooo");
+                    break;
+            }
+
+        }
+    
+    }
+}
+
