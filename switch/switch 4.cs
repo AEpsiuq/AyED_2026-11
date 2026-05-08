67a75a49a498173fbf85ce3b901a72ae7ce88b85
AEpsiuq <aliss.q1i5@gmail.com>
2026-05-08 16:30:28 -0300

    Add files via upload

---
+++ switch/switch 4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write("Ingresar un número: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                if (num > 100)
+                {
+                    contador++;
+                }
+            }
+
+            Console.WriteLine("Cantidad mayores a 100: " + contador);
+        }
+    }
+}
+
+    
