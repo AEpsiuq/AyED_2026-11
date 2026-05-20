8769974f1b62e93fb7a39d10efcd7283e38a542a
AEpsiuq <aliss.q1i5@gmail.com>
2026-05-20 14:27:03 -0300

    Add files via upload

---
+++ while/while1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whiledowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar un numero: ");
+            int numero = int.Parse(Console.ReadLine());
+            int contador = 0;
+
+            Console.WriteLine("Comienza la cuenta regresiva: ");
+
+            while (numero > -1)
+            {
+                Console.WriteLine("Numero: " + numero);
+                numero--;
+                contador++;
+            }
+            Console.WriteLine("Numeros contados: " + contador);
+        }
+    }
+}
