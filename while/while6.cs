b9b27fc175f7c372a2960a4b2633350e69e8f9fd
AEpsiuq <aliss.q1i5@gmail.com>
2026-05-22 16:48:25 -0300

    Add files via upload

---
+++ while/while6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número límite: ");
+            int limite = Convert.ToInt32(Console.ReadLine());
+
+            int i = 1;
+
+            while (i <= limite)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+                i++;
+            }
+        }
+    }
+    
+}
+
+
+
+
+
