e6fef6d1838f5fe4ae5aa4b00ee28f22f5d50f79
AEpsiuq <aliss.q1i5@gmail.com>
2026-05-22 14:43:35 -0300

    Add files via upload

---
+++ while/while3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1;
+            int acumulador = 0;
+
+            while (numero != 0)
+            {
+                Console.Write("Ingrese un número: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                acumulador = acumulador + numero;
+            }
+
+            Console.WriteLine("El total acumulado es de: " + acumulador);
+        }
+    }
+
+}
+
