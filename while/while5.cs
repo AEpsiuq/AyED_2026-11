4e8d76bbdf0a59ed5ccc1512625f2547a26742f8
AEpsiuq <aliss.q1i5@gmail.com>
2026-05-22 16:30:33 -0300

    Add files via upload

---
+++ while/while5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string clave = "";
+
+            while (clave != "6767")
+            {
+                Console.Write("Ingresar clave de acceso: ");
+                clave = Console.ReadLine();
+            }
+
+            Console.WriteLine("Acceso permitido"); 
+        }
+    }
+}
+
+
+
+
+
