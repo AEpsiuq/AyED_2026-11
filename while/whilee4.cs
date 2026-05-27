a0e7338e8e4f34a215c2a2d99b6107f8de79dacb
AEpsiuq <aliss.q1i5@gmail.com>
2026-05-27 16:41:49 -0300

    Add files via upload

---
+++ while/whilee4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar un número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            int i = 1;
+
+            while (i <= 12)
+            {
+                Console.WriteLine(numero  + " x " + i + " = " + (numero * i));
+                i++;
+            }
+        }
+    }
+}
+
+
