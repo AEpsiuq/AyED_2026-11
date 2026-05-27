5a427151af7d1417e52e285f13eff007b2de0384
AEpsiuq <aliss.q1i5@gmail.com>
2026-05-27 15:37:50 -0300

    Add files via upload

---
+++ while/while8.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+
+            while (opcion != 3)
+            {
+                Console.WriteLine("1 - Saludar");
+                Console.WriteLine("2 - Despedirse");
+                Console.WriteLine("3 - Salir");
+
+                Console.Write("Ingresar una opción: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Hola");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Adiós");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Programa finalizado");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción inválida");
+                        break;
+                }
+            }
+        }
+    }
+}
+
