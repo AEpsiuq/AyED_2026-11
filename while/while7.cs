6018503347d9a8f8b3f177d932a5fa2b911341a0
AEpsiuq <aliss.q1i5@gmail.com>
2026-05-27 15:40:23 -0300

    Add files via upload

---
+++ while/while7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese una temperatura: ");
+
+            int temperatura = int.Parse(Console.ReadLine());
+
+            while (temperatura > 24 )
+            {
+                temperatura = temperatura - 2;
+                Console.WriteLine("Enfriando...Temperatura actual: " + temperatura + "°C.");
+
+            }
+        }
+    }
+}
