869426120a7c57120f0cadfc761c7d525a86cd91
AEpsiuq <aliss.q1i5@gmail.com>
2026-05-27 15:45:20 -0300

    Add files via upload

---
+++ while/whilee7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+            int i = 1;
+
+            while (i <= 5)
+            {
+                Console.Write("Ingresar nota: ");
+                int nota = Convert.ToInt32(Console.ReadLine());
+
+                acumulador = acumulador + nota;
+
+                i++;
+            }
+
+            int promedio = acumulador / 5;
+
+            Console.WriteLine("Promedio: " + promedio);
+
+        }
+    }
+}
+
