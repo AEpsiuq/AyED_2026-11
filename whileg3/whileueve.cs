34a5dd357010c6d65cd9db5f9ad2724324e0757a
AEpsiuq <aliss.q1i5@gmail.com>
2026-06-12 17:03:08 -0300

    Add files via upload

---
+++ whileg3/whileueve.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingresar un número positivo: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            int menor = numero;
+
+            while (numero >= 0)
+            {
+                if (numero < menor)
+                {
+                    menor = numero;
+                }
+
+                Console.Write("Ingrese otro número (negativo para terminar): ");
+                numero = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("El menor número es: " + menor);
+
+        }
+    }
+}
