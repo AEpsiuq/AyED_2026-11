f89435c04235db1060982fc32c4e4c2c99f663be
AEpsiuq <aliss.q1i5@gmail.com>
2026-08-19 14:35:43 -0300

    Add files via upload

---
+++ vectores-arrays/arrays1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] temperaturas = new double[7];
+        
+            double suma = 0;
+            double mayor;
+            double menor;
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write("Ingresar la temperatura del día " + (i + 1) + ": ");
+                temperaturas[i] = Convert.ToDouble(Console.ReadLine());
+
+                suma = suma + temperaturas[i];
+            }
+
+            mayor = temperaturas[0];
+            menor = temperaturas[0];
+
+            for (int i = 1; i < 7; i++)
+            {
+                if (temperaturas[i] > mayor)
+                {
+                    mayor = temperaturas[i];
+                }
+
+                if (temperaturas[i] < menor)
+                {
+                    menor = temperaturas[i];
+                }
+            }
+
+            double promedio = suma / 7;
+
+            Console.WriteLine("Temperatura más alta: " + mayor);
+            Console.WriteLine("Temperatura más baja: " + menor);
+            Console.WriteLine("Promedio de la semana: " + promedio);
+
+        }
+    }
+}
---
+++ vectores-arrays/arrays2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] notas = new int[10];
+     
+            int aprobados = 0;
+            int desaprobados = 0;
+            int suma = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Ingresar nota del alumnado: " + (i + 1) + ": ");
+                notas[i] = Convert.ToInt32(Console.ReadLine());
+
+                suma = suma + notas[i];
+
+                if (notas[i] >= 6)
+                {
+                    aprobados++;
+                }
+                else
+                {
+                    desaprobados++;
+                }
+            }
+
+            double promedio = suma / 10.0;
+
+            Console.WriteLine("Aprobados: " + aprobados);
+            Console.WriteLine("Desaprobados: " + desaprobados);
+            Console.WriteLine("Promedio: " + promedio);
+
+        }
+    }
+}
