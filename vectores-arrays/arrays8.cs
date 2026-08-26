6569591778776a1282c8ee556a15ef173d45570e
AEpsiuq <aliss.q1i5@gmail.com>
2026-08-26 16:03:43 -0300

    Add files via upload

---
+++ vectores-arrays/arrays8.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] respuestas = new int[25];
+
+            int cantidad1 = 0;
+            int cantidad2 = 0;
+            int cantidad3 = 0;
+            int cantidad4 = 0;
+            int cantidad5 = 0;
+
+            for (int i = 0; i < 25; i++)
+            {
+                Console.Write("Ingresar calificacion dl cliente: " + (i + 1) + " (1 a 5): ");
+                respuestas[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (respuestas[i] == 1)
+                {
+                    cantidad1++;
+                }
+                else if (respuestas[i] == 2)
+                {
+                    cantidad2++;
+                }
+                else if (respuestas[i] == 3)
+                {
+                    cantidad3++;
+                }
+                else if (respuestas[i] == 4)
+                {
+                    cantidad4++;
+                }
+                else if (respuestas[i] == 5)
+                {
+                    cantidad5++;
+                }
+            }
+
+            Console.WriteLine("Cantidad de respuestas 1: " + cantidad1);
+            Console.WriteLine("Cantidad de respuestas 2: " + cantidad2);
+            Console.WriteLine("Cantidad de respuestas 3: " + cantidad3);
+            Console.WriteLine("Cantidad de respuestas 4: " + cantidad4);
+            Console.WriteLine("Cantidad de respuestas 5: " + cantidad5);
+
+        }
+    }
+}
---
+++ vectores-arrays/arrays9.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double[] precios = new double[10];
+
+            double total = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Ingrese precio del producto " + (i + 1) + ": ");
+                precios[i] = Convert.ToDouble(Console.ReadLine());
+
+                total = total + precios[i];
+            }
+
+            Console.Write("Ingrese la posición del producto a devolver (1 a 10): ");
+            int posicion = Convert.ToInt32(Console.ReadLine());
+
+            double precioDevuelto = precios[posicion - 1];
+
+            total = total - precioDevuelto;
+
+            Console.WriteLine("Precio del producto devuelto: $" + precioDevuelto);
+            Console.WriteLine("Nuevo total: $" + total);
+
+
+        }
+    }
+}
