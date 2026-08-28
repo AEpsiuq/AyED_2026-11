35f3dcd69e4639c1e0f28d1070c2c2c00a794776
AEpsiuq <aliss.q1i5@gmail.com>
2026-08-28 15:28:25 -0300

    Add files via upload

---
+++ funciones/funciones2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número entero positivo: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            ContarHasta(numero);
+        }
+
+        static void ContarHasta(int numero)
+        {
+            int contador = 1;
+
+            while (contador <= numero)
+            {
+                Console.WriteLine(contador);
+                contador++;
+            }
+        }
+    }
+}
+
+
