4c9c85b8f011e6ba128ef98ad9d15569e6e30c8e
AEpsiuq <aliss.q1i5@gmail.com>
2026-08-28 14:30:42 -0300

    Add files via upload

---
+++ funciones/funcion1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar un numero entero positivo: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            ContarRegresivo(numero);
+        }
+        static void ContarRegresivo(int numero)
+        {
+            while (numero >= 1)
+            {
+                numero--;
+                Console.WriteLine(numero);
+            }
+        }
+    
+    }
+}
