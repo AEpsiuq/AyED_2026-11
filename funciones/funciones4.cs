cbf67076987fe38ccc43c1acc2d9fdafa5000b92
AEpsiuq <aliss.q1i5@gmail.com>
2026-08-28 15:54:23 -0300

    Add files via upload

---
+++ funciones/funciones4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar un número entero mayor o igual a 0: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = Factorial(numero);
+
+            Console.WriteLine("El factorial es: " + resultado);
+        }
+        static int Factorial(int numero)
+        {
+            int resultado = 1;
+
+            for (int i = 1; i <= numero; i++)
+            {
+                resultado = resultado * i;
+            }
+
+            return resultado;
+        }
+    }
+}
+
+
